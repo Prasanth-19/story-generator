17899f9a5d6880540e43f2135e1658522de3b89a
Prasanth Kumar Vanapalli <113783560+Prasanth-19@users.noreply.github.com>
2025-11-21 15:55:30 +0530

    Change app port from 5000 to 7860

--- app.py
+++ app.py
@@ -96,4 +96,5 @@ def internal_error(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
